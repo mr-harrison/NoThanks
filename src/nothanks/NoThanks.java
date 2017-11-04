7d8c2254225166b714dfec589ccf59a6420282e8
user <user@10.152.52.158>
2017-11-04 01:35:19 -0500

    Card game commit

---
+++ src/nothanks/NoThanks.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nothanks;
+
+/**
+ *
+ * @author user
+ */
+public class NoThanks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StartMenuGUI sm = new StartMenuGUI();
+        sm.setVisible(true);
+    }
+    
+}
